2d839515baf2a8a39a9e088a8c1715f8be8d3a9f
liuchangy <liuchangy@yonyou.com>
2018-03-19 20:38:55 +0800

    迅雷远程自动登录完善

--- config.py
+++
@@ -1,8 +0,0 @@
-thunder = {
-    'username': '18501357462',
-    'password': 'as47154545',
-    'cookie_path': 'cookie.txt',
-    'verification_image_path': './vcode.jpg'
-}
-mydir = '/volume1/homes/admin/python/thunder'
-
